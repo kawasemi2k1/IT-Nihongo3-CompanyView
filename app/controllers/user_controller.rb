6a92e4c7dda0676efe198d2bcb146417baf88fbc
namnt0413 <namnt0413@gmail.com>
2023-02-08 17:31:06 +0000

    feat: user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,37 @@
 class UserController < ApplicationController
-  def index
-  end
+    before_action :authenticate_user!, only: %i[ show edit update ]
 
-  def new
+  def index
   end
 
   def show
   end
 
-  def create
-  end
-
   def edit
+      @user = current_user
   end
 
   def update
+        @user= current_user
+   if @user.update_attributes(user_params)
+    # Handle a successful update.
+    render 'show'
+  else
+    render 'show'
+  end
   end
 
   def destroy
+    # respond_to do |format|
+    #   format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+    #   format.json { head :no_content }
+    # end
   end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:email, :name, :age , :avatar)
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'admin/index'
   get 'user/index'
-  get 'user/new'
   get 'user/show'
-  get 'user/create'
   get 'user/edit'
-  get 'user/update'
+  put 'user/update'
   get 'user/destroy'
   get 'static_pages/home'
   root 'static_pages#home'
