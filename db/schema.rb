13e4a1238fbf32327eb237b7c4f23b78e3727f9a
PhamNhatSang20194657 <sang.pn194657@sis.hust.edu.vn>
2023-02-10 10:41:55 +0700

    add_company

--- db/schema.rb
+++ db/schema.rb
@@ -48,5 +48,17 @@ ActiveRecord::Schema.define(version: 2023_02_08_152050) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "programming_langue"
+    t.string "vacancy"
+    t.string "location"
+    t.string "website_link"
+    t.string "description"
+    t.string "desc_detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
