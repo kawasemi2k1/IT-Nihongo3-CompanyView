a40b3bf20614d0bd9d054e31847b46cf51e2ad7a
hiep464 <luuhiep2309@gmail.com>
2023-02-10 17:39:10 +0000

    decentralization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_08_152050) do
+ActiveRecord::Schema.define(version: 2023_02_10_165421) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 2023_02_08_152050) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "programming_langue"
+    t.string "vacancy"
+    t.string "location"
+    t.string "website_link"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,21 +54,10 @@ ActiveRecord::Schema.define(version: 2023_02_08_152050) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "age"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-    t.string "programming_langue"
-    t.string "vacancy"
-    t.string "location"
-    t.string "website_link"
-    t.string "description"
-    t.string "desc_detail"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
