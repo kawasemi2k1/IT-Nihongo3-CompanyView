2eb1ea2dde9811e6ac969bfcdaee00b9c2889ee9
hmyng <uonghongminh07@gmail.com>
2023-02-12 11:12:32 +0000

    fix logout

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   root 'static_pages#home'
   resources :companies
-  devise_for :users
+  devise_for :users do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
